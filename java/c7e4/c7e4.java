57489dde9924b2ac19e5398fd22428772c834bc4
David Dehoust <dehoustdavid@gmail.com>
2022-09-02 11:44:19 +0200

    array

--- java/c7e4/c7e4.java
+++ java/c7e4/c7e4.java
@@ -1,4 +1,14 @@
 public class c7e4 {
     public static void main(String[] args) {
+
+        String[] players = { "u1", "u2", "u3" };
+
+        System.out.println(players[0]);
+        System.out.println(players[1]);
+        System.out.println(players[2]);
+
+        for (int i = 0; i < players.length; i++) {
+            System.out.println(players[i]);
+        }
     }
 }
