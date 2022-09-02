cb34ad7709c722bdaa3712a5f06c4ce3a8f5fda2
David Dehoust <dehoustdavid@gmail.com>
2022-09-02 13:47:39 +0200

    firststep

---
+++ java/c7e4/ArrayReminder.java
@@ -0,0 +1,15 @@
+public class ArrayReminder {
+
+    public static void main(String[] args) {
+
+        String[] players = { "u1", "u2", "u3" };
+
+        System.out.println(players[0]);
+        System.out.println(players[1]);
+        System.out.println(players[2]);
+
+        for (int i = 0; i < players.length; i++) {
+            System.out.println(players[i]);
+        }
+    }
+}
--- java/c7e4/c7e4.java
+++ java/c7e4/c7e4.java
@@ -2,18 +2,30 @@ import java.util.Scanner;
 
 public class c7e4 {
 
-    static Scanner input=new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        String[] players = { "u1", "u2", "u3" };
+        int score = 0;
+        //score = Qmult("combien de pattes ont les chats ?", 3, ("4", "2","3","4"));
 
-        System.out.println(players[0]);
-        System.out.println(players[1]);
-        System.out.println(players[2]);
+    }
 
-        for (int i = 0; i < players.length; i++) {
-            System.out.println(players[i]);
+    // cette fonction va poser une question, la question est en paramètre.
+    // la réponse à la question est également un paramètre.
+    // les question à choix multiple sont composée de 4 propositions
+
+    static int Qmult(String _Question, int _responce, String[] _case) {
+
+        String _r = "";
+
+        System.out.println("question ?\n[" + _case[0] + "]\n[" + _case[1] + "]\n[" + _case[2] + "]\n[" + _case[3] + "]\n");
+
+
+        if (_r == "") {
+            return 2;
+        } else {
+            return 0;
         }
     }
 }
