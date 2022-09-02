4cc9fe6e8ecf71dd08f0afc36f37e0dc52fa33f5
David Dehoust <dehoustdavid@gmail.com>
2022-09-02 14:29:23 +0200

    question 4 7 10 13 16 19

--- java/c7e4/c7e4.java
+++ java/c7e4/c7e4.java
@@ -2,38 +2,116 @@ import java.util.Scanner;
 
 public class c7e4 {
 
-    // reponse direct
-    // vrais ou faux
-    // QCM
-
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        int score = 0;
-        System.out.println("question 1");
-<<<<<<< HEAD
-        String r1 = input.nextLine();
-        String rc1 = "";
 
-        if (r1.equals(rc1)) {
-            score = 3;
-=======
-        String r1=input.nextLine();
-        String rc1="";
-        if (r1.equals(rc1)) {
-            score=3;
->>>>>>> 95ee01b563fcecc85e3d99384fceb1e81f1444ca
-            System.out.println("Vous gagnez 3 points.");
+
+    // partie de David
+
+    // 1, 4, 7, 10, 13, 16, 19 reponse direct
+    // 2, 5, 8, 11, 14, 17, 20 vrais ou faux
+    // 3, 6, 9, 12, 15 ,18, QCM
+
+        int score = 0; // doublon a effacer
+
+        // question 2
+
+        // question 3
+
+        // question 4
+
+        System.out.println("question 4");
+        String r4 = input.nextLine();
+        String rc4 = "";
+        if (r4.equals(rc4)) {
+            score = 0; // a modifié
+            System.out.println("vous gagnez 3 points");
+        } else {
+            System.out.println("Réponse incorrect !\nProchaine question...");
         }
 
+        // question 5
 
+        // question 6
 
+        // question 7
 
-        // partie de David
+        System.out.println("question 7");
+        String r7 = input.nextLine();
+        String rc7 = "";
+        if (r7.equals(rc7)) {
+            score = 0; // a modifié
+            System.out.println("vous gagnez 3 points");
+        } else {
+            System.out.println("Réponse incorrect !\nProchaine question...");
+        }
 
+        // question 8
 
+        // question 9
 
+        // question 10
+        System.out.println("question 10");
+        String r10 = input.nextLine();
+        String rc10 = "";
+        if (r10.equals(rc10)) {
+            score = 0; // a modifié
+            System.out.println("vous gagnez 3 points");
+        } else {
+            System.out.println("Réponse incorrect !\nProchaine question...");
+        }
+
+        // question 11
+
+        // question 12
+
+        // question 13
+
+        System.out.println("question 13");
+        String r13 = input.nextLine();
+        String rc13 = "";
+        if (r13.equals(rc13)) {
+            score = 0; // a modifié
+            System.out.println("vous gagnez 3 points");
+        } else {
+            System.out.println("Réponse incorrect !\nProchaine question...");
+        }
+
+        // question 14
+
+        // question 15
+
+        // question 16
+
+        System.out.println("question 16");
+        String r16 = input.nextLine();
+        String rc16 = "";
+        if (r16.equals(rc16)) {
+            score = 0; // a modifié
+            System.out.println("vous gagnez 3 points");
+        } else {
+            System.out.println("Réponse incorrect !\nProchaine question...");
+        }
+
+        // question 17
+
+        // question 18
+
+        // question 19
+
+        System.out.println("question 19");
+        String r19 = input.nextLine();
+        String rc19 = "";
+        if (r19.equals(rc19)) {
+            score = 0; // a modifié
+            System.out.println("vous gagnez 3 points");
+        } else {
+            System.out.println("Réponse incorrect !\nProchaine question...");
+        }
+
+        // question 20
 
     }
 
