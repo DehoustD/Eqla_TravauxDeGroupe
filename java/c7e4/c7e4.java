a01817d001cd890d11c12edcb83658d972d5fc9d
David Dehoust <dehoustdavid@gmail.com>
2022-09-02 10:39:22 +0200

    DDtest2

--- java/c7e4/c7e4.java
+++ java/c7e4/c7e4.java
@@ -2,5 +2,6 @@ public class c7e4 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("DDtest");
+        System.out.println("DDtest2");
     }
 }
