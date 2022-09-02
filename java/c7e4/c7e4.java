dbe0ddeaf818e6a8e448a88bb44fac1c6c9ea424
David Dehoust <dehoustdavid@gmail.com>
2022-09-02 11:45:15 +0200

    merge1

--- java/c7e4/c7e4.java
+++ java/c7e4/c7e4.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+
 public class c7e4 {
-    Scanner input=new Scanner(System.in);
+
+    static Scanner input=new Scanner(System.in);
+
     public static void main(String[] args) {
 
         String[] players = { "u1", "u2", "u3" };
