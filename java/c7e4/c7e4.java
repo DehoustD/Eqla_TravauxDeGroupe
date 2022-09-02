eac97c643e9def2d2397ecb34ad127fa7777edef
David Dehoust <dehoustdavid@gmail.com>
2022-09-02 11:18:56 +0200

    retrait des DD test

--- java/c7e4/c7e4.java
+++ java/c7e4/c7e4.java
@@ -1,10 +1,7 @@
 public class c7e4 {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("DDtest");
-        System.out.println("DDtest2");
         System.out.println("Test 3");
         System.out.println("tourte");
-        System.out.println("DDtest3");
     }
 }
